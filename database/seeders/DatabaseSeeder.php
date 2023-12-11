3a73a1e8464b3f40f3d9319858845924683f21bf
Leonardo Coelho <leonardocoel98@gmail.com>
2023-12-11 22:23:53 +0000

    feat: adjust user factory and create database seeder

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
+use App\Models\User;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $now = Carbon::now();
+
+        DB::table('roles')->insert([
+            ['role_name' => 'root_admin', 'created_at' => $now, 'updated_at' => $now],
+            ['role_name' => 'cinema_admin', 'created_at' => $now, 'updated_at' => $now],
+            ['role_name' => 'end_user', 'created_at' => $now, 'updated_at' => $now],
+        ]);
+
+        $rootAdmin = User::factory()->create([
+            'email' => 'root_admin@mail.com',
+        ]);
+
+        $rootAdminId = DB::table('roles')->where('role_name', 'root_admin')->value('id');
+
+        DB::table('user_roles')->insert([
+            'user_id' => $rootAdmin->id,
+            'role_id' => $rootAdminId,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+    }
+}
