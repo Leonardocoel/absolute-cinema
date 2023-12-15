938877cbf6199c999435c1428049db7f07254cd8
Leonardo Coelho <leonardocoel98@gmail.com>
2023-12-15 12:50:44 +0000

    build: inertia configuration

---
+++ resources/js/Components/SecondaryButton.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+    type: {
+        type: String,
+        default: "button",
+    },
+});
+</script>
+
+<template>
+    <button
+        :type="type"
+        class="px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase shadow-sm hover:bg-gray-50 disabled:opacity-25"
+    >
+        <slot />
+    </button>
+</template>
