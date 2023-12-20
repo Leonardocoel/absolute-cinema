e5e8badb0f732443a99016347e0ba1be245a0225
Leonardo Coelho <leonardocoel98@gmail.com>
2023-12-20 18:59:21 +0000

    Refactor: report controllers adjustments

--- app/Http/Controllers/CinemaAdmin/ReportController.php
+++ app/Http/Controllers/CinemaAdmin/ReportController.php
@@ -9,9 +9,6 @@ use App\Models\Movie;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Storage;
-use App\Http\Requests\StoreMovieRequest;
-use App\Http\Requests\UpdateMovieRequest;
 
 class ReportController extends Controller
 {
@@ -19,7 +16,7 @@ class ReportController extends Controller
     public function index()
     {
         $user = User::find(Auth::id())->load(['cinemas']);
-        $cinema = $user->cinemas[0];
+        $cinema = $user->cinemas[0];;
 
         try {
             $query = DB::table('movies as m')
@@ -30,35 +27,17 @@ class ReportController extends Controller
                 ->select('m.id', 'm.title', 'm.release_date', DB::raw('COUNT(m.id) as views'), DB::raw('SUM(price) as total'))
                 ->where('s.cinema_id', '=', $cinema->id)
                 ->groupBy('m.id');
-
-
-            // cast data
-
-            $movies = $query->orderByDesc('m.release_date')->get();
-
-            $moviesWeek = $query->where('start_time', '>=', now()->subDays(7))->get();
-
-            return Inertia::render('CinemaAdmin/Dashboard', [
-                'movies' => $movies,
-                'moviesWeek' => $moviesWeek,
-            ]);
         } catch (\Exception $e) {
             dd($e);
         }
-    }
 
+        $movies = $query->orderByDesc('m.release_date')->get();
 
-    public function show(Movie $filme)
-    {
-        try {
+        $moviesWeek = $query->where('start_time', '>=', now()->subDays(7))->get();
 
-            $filme->release_date_raw = Carbon::parse($filme->release_date)->format('Y-m-d');
-
-            return Inertia::render('CinemaAdmin/Movie/Show', [
-                'movie' => $filme,
-            ]);
-        } catch (\Exception $e) {
-            dd($e);
-        }
+        return Inertia::render('CinemaAdmin/Dashboard', [
+            'movies' => $movies,
+            'moviesWeek' => $moviesWeek,
+        ]);
     }
 }
--- app/Http/Controllers/RootAdmin/ReportController.php
+++ app/Http/Controllers/RootAdmin/ReportController.php
@@ -2,51 +2,51 @@
 
 namespace App\Http\Controllers\RootAdmin;
 
-use Carbon\Carbon;
-use App\Models\User;
 use Inertia\Inertia;
 use App\Models\Movie;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Storage;
-use App\Http\Requests\StoreMovieRequest;
-use App\Http\Requests\UpdateMovieRequest;
+
 
 class ReportController extends Controller
 {
-    public function __construct()
-    {
-        // $this->authorizeResource(Movie::class, 'filme');
-    }
 
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
 
         try {
-            $query = DB::table('movies as m')
-                ->join('session_schedule as ss', 'ss.movie_id', '=', 'm.id')
-                ->join('schedules as sc', 'sc.id', '=', 'ss.schedule_id')
-                ->join('sessions as s', 's.id', '=', 'ss.session_id')
-                ->join('reservations as r', 'r.session_schedule_id', '=', 'ss.id')
-                ->select('m.id', 'm.title', 'm.release_date', DB::raw('COUNT(m.id) as views'), DB::raw('SUM(price) as total'))
-                ->groupBy('m.id');
+            $movies = Movie::select(['id', 'title', 'release_date'])
+                ->withCount('reservations as views')
+                ->withSum('reservations as total', 'price')
+                ->orderByDesc('release_date')->get();
 
-            //* cast release_date
 
-            $movies = $query->orderByDesc('m.release_date')->get();
-
-            $moviesWeek = $query->where('start_time', '>=', now()->subDays(7))->get();
-
-            return Inertia::render('RootAdmin/Dashboard', [
-                'movies' => $movies,
-                'moviesWeek' => $moviesWeek,
-            ]);
+            $moviesWeek = Movie::select(['id', 'title', 'release_date'])->whereRelation('schedules', 'start_time', '>=', now()->subDays(7))
+                ->withCount(['reservations as views' => fn ($q) => $q
+                    ->whereRelation('sessionSchedule.schedule', 'start_time', '>=', now()->subDays(7))])
+                ->withSum(['reservations as total' => fn ($q) => $q
+                    ->whereRelation('sessionSchedule.schedule', 'start_time', '>=', now()->subDays(7))], 'price')
+                ->orderByDesc('views')->get();
         } catch (\Exception $e) {
             dd($e);
         }
+
+        return Inertia::render('RootAdmin/Dashboard', [
+            'movies' => $movies,
+            'moviesWeek' => $moviesWeek,
+        ]);
     }
 }
+
+
+ // $query = DB::table('movies as m')
+        //     ->join('session_schedule as ss', 'ss.movie_id', '=', 'm.id')
+        //     ->join('schedules as sc', 'sc.id', '=', 'ss.schedule_id')
+        //     ->join('reservations as r', 'r.session_schedule_id', '=', 'ss.id')
+        //     ->select('m.id', 'm.title', 'm.release_date', DB::raw('COUNT(m.id) as views'), DB::raw('SUM(price) as total'))
+        //     ->groupBy('m.id');
+
+
+        // $movies = $query->orderByDesc('m.release_date')->get();
+
+        // $moviesWeek = $query->where('start_time', '>=', now()->subDays(7))->get();
