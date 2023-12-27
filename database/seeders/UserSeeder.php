d1fa137d4a9fd8496b3b06f3cf6a4d97daa8db51
Leonardo Coelho <leonardocoel98@gmail.com>
2023-12-27 01:33:23 +0000

    refactor: database seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,42 +2,17 @@
 
 namespace Database\Seeders;
 
-use Carbon\Carbon;
-use App\Models\Role;
+
 use App\Models\User;
-use App\Models\Cinema;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-
-        $now = Carbon::now();
-
-        DB::table('roles')->insert([
-            ['role_name' => 'root_admin', 'created_at' => $now, 'updated_at' => $now],
-            ['role_name' => 'cinema_admin', 'created_at' => $now, 'updated_at' => $now],
-            ['role_name' => 'end_user', 'created_at' => $now, 'updated_at' => $now],
-        ]);
-
-        $roles = DB::table('roles')->get();
-
-
-
-        $adminId = $roles->where('role_name', 'cinema_admin')->value('id');
-        $endUserId = $roles->where('role_name', 'end_user')->value('id');
-        $rootAdminId = $roles->where('role_name', 'root_admin')->value('id');
-
-        $cinema = Cinema::factory()->create();
-
-        User::factory()->hasAttached(Role::class, ['role_id' => $rootAdminId])->create();
-        User::factory()->hasAttached(Role::class, ['role_id' => $adminId, 'cinema_id' => $cinema->id])->create();
-        User::factory()->hasAttached(Role::class, ['role_id' => $endUserId, 'cinema_id' => $cinema->id])->create();
+        User::factory()->create(["email" => "admin@mail.com", 'role' => 'root_admin']);
+        User::factory()->create(["email" => "cinema@mail.com", 'role' => 'admin']);
+        User::factory()->create(["email" => "usuario@mail.com", 'role' => 'end_user']);
     }
 }
